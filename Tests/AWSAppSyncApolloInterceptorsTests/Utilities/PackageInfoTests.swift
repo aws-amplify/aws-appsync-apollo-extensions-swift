3dd850d9a9d051a26ee8ab90f9cf7716ae87e997
Di Wu <diwud@amazon.com>
2024-08-19 14:03:11 -0700

    feat: add user-agent header to graphql requests

---
+++ Tests/AWSAppSyncApolloInterceptorsTests/Utilities/PackageInfoTests.swift
@@ -0,0 +1,28 @@
+//
+// Copyright Amazon.com Inc. or its affiliates.
+// All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+
+
+import XCTest
+@testable import AWSAppSyncApolloInterceptors
+
+class PackageInfoTests: XCTestCase {
+
+    @available(iOS 16.0, *)
+    /// user agent header has format of:
+    /// UA/2.0 lang/swift/x.x(.x) os/[iOS, macOS, watchOS]/x.x(.x) lib/aws-appsync-apollo-extensions-swift/x.x.x
+    func testUserAgentHasCorrectFormat() async throws {
+        let format = try Regex(
+            "^UA/2\\.0 " +
+            "lang/swift/\\d+\\.\\d+(?:\\.\\d+)? " +
+            "os/iOS|macOS|watchOS/\\d+\\.\\d+(?:\\.\\d+)? " +
+            "lib/aws-appsync-apollo-extensions-swift/\\d+\\.\\d+\\.\\d+$"
+        )
+        let userAgent = await PackageInfo.userAgent
+        let matches = userAgent.ranges(of: format)
+        XCTAssertTrue(!matches.isEmpty)
+    }
+}
