101fe0c1f385b93fc6aae11e1f44bb808613aa2e
aws-amplify-ops <aws-amplify-ops@amazon.com>
2024-09-27 22:28:07 +0000

    [bump version 1.0.1]

--- Sources/AWSAppSyncApolloExtensions/Utilities/PackageInfo.swift
+++ Sources/AWSAppSyncApolloExtensions/Utilities/PackageInfo.swift
@@ -21,7 +21,7 @@ import AppKit
 
 class PackageInfo {
 
-    private static let version = "1.0.0"
+    private static let version = "1.0.1"
 
     @MainActor
     private static var os: (name: String, version: String) = {
